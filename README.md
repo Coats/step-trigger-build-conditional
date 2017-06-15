0c978ef0a0fd3211b3cf2b354bcb4ef867ec1bc8
Petrica Martinescu <pmartinescu@tremend.ro>
2017-06-16 02:40:20 +0300

    Initial draft

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# step-trigger-build-conditional
-A wercker step to trigger another build if git message contains specific keywords or git folder has changes.
+# Trigger Build Conditional
+
+A wercker step to trigger another build if git message contains specific keywords or git path has changes.
+
+On error the response JSON is returned in WBTC_TRIGGER_RESPONSE env variable.
+
+You will need to create a personal API access token for wercker
+
+Environment variable WERCKER_APPLICATION_ID is provided by default.
+
+Example:
+
+    build:
+      steps:
+        - petrica/trigger-build:
+            git-message: "build-keyword" # only build if commit contains message
+            git-path: "src/" # only build on src path changes
+            application-id: $WERCKER_APPLICATION_ID
+            pipeline-id: $WERCKER_PIPELINE_ID
+            token: $WERCKER_API_TOKEN
--- run.sh
+++ run.sh
@@ -3,12 +3,11 @@
 set -e
 
 WBTC_MESSAGE=$(git log -1 --pretty=%s)
-echo "Commit message: $WBTC_MESSAGE"
 
 WBTC_TRIGGER=false
 
-if [ -n "$WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_FOLDER" ] && [ $(git show $WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_FOLDER | wc -c) -ne 0 ]; then
-  echo "Changed detected on folder $WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_FOLDER."
+if [ -n "$WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_PATH" ] && [ $(git show $WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_PATH | wc -c) -ne 0 ]; then
+  echo "Changed detected on path $WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_PATH."
   WBTC_TRIGGER=true
 fi
 
@@ -17,22 +16,21 @@ if [ -n "$WERCKER_TRIGGER_BUILD_CONDITIONAL_GIT_MESSAGE" ] && [[ "$WBTC_MESSAGE"
   WBTC_TRIGGER=true
 fi
 
-if [ "WBTC_TRIGGER" = "true" ]; then
-  WTB_ENDPOINT="https://app.wercker.com/api/v3/runs/"
+if [ "$WBTC_TRIGGER" = "true" ]; then
+  WBTC_ENDPOINT="https://app.wercker.com/api/v3/runs/"
 
 
-  WTB_JSON="{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_APPLICATION_ID\", \
-  \"pipelineId\": \"$WERCKER_TRIGGER_BUILD_PIPELINE_ID\", \
+  WBTC_JSON="{\"applicationId\": \"$WERCKER_TRIGGER_BUILD_CONDITIONAL_APPLICATION_ID\", \
+  \"pipelineId\": \"$WERCKER_TRIGGER_BUILD_CONDITIONAL_PIPELINE_ID\", \
   \"branch\": \"$WERCKER_GIT_BRANCH\", \
   \"sourceRunId\": \"$WERCKER_RUN_ID\", \
-  \"message\": \"$(git log -1 --pretty=%s)\"}"
-  echo "$WTB_JSON"
-  echo "Calling $WTB_ENDPOINT"
+  \"message\": \"$WBTC_MESSAGE\"}"
+  echo "$WBTC_JSON"
+  echo "Calling $WBTC_ENDPOINT"
 
-  export WERCKER_TRIGGER_RESPONSE=$(curl -s -k -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_TRIGGER_BUILD_TOKEN" "$WTB_ENDPOINT" -d "$WTB_JSON" | grep \"error\")
+  export WBTC_TRIGGER_RESPONSE=$(curl -s -k -H "Content-type: application/json" -H "Authorization: Bearer $WERCKER_TRIGGER_BUILD_CONDITIONAL_TOKEN" "$WTBC_ENDPOINT" -d "$WTBC_JSON" | grep \"error\")
 
-  if [ ! -z "$WERCKER_TRIGGER_RESPONSE" ]; then
-    echo "$WERCKER_TRIGGER_RESPONSE"
-    exit 1
+  if [ ! -z "$WBTC_TRIGGER_RESPONSE" ]; then
+    faild "$WBTC_TRIGGER_RESPONSE"
   fi
 fi
